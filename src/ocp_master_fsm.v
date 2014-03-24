3fe3a85d3a69de97c99da8113be886f8c9af3f48
Michael Walton <spesialstyrker@gmail.com>
2014-03-24 11:15:01 -0700

    Changed to 3 state FSM to reflect OCP 3.0 spec

--- src/ocp_master_fsm.v
+++ src/ocp_master_fsm.v
@@ -18,8 +18,7 @@
 * 1 CYCLE LONG
 *
 * datahandshake phase ends when SDataAccept is sampled as high
-*/
-
+*/ 
 //`include "const.vh"
 `define MDATA_WIDTH 8
 `define SDATA_WIDTH 8
@@ -28,13 +27,13 @@
 module ocp_master_fsm(
   // Bridge interface/*{{{*/
   input wire [`MADDR_WIDTH - 1:0] address,
-  input wire                      data_valid,
+  //input wire                      data_valid,
   input wire                      read_request,
   input wire                      reset,
   input wire [`MDATA_WIDTH - 1:0] write_data,   // Coming from PCIe side
   input wire                      write_request,
 
-  output reg [`MDATA_WIDTH - 1:0] read_data,    // Coming from OCP bus
+  output reg [`SDATA_WIDTH - 1:0] read_data,    // Coming from OCP bus
   /*}}}*/
 
   // OCP 3.0 interface/*{{{*/
@@ -42,13 +41,13 @@ module ocp_master_fsm(
   input wire                      EnableClk,
   input wire                      SCmdAccept,
   input wire [`SDATA_WIDTH - 1:0] SData,
-  input wire                      SDataAccept,
+  //input wire                      SDataAccept,
   input wire [1:0]                SResp,
 
   output reg [`MADDR_WIDTH - 1:0] MAddr,
   output reg [2:0]                MCmd,
-  output reg [`MDATA_WIDTH - 1:0] MData,
-  output reg                      MDataValid
+  output reg [`MDATA_WIDTH - 1:0] MData
+  //output reg                      MDataValid
   /*}}}*/
 );
 
@@ -66,23 +65,19 @@ parameter BCST  = 3'b111;
 
 // SResp states
 parameter NULL  = 2'b00;
-parameter DVA   = 2'b00;
-parameter FAIL  = 2'b00;
-parameter ERR   = 2'b00;
+parameter DVA   = 2'b01;
+parameter FAIL  = 2'b10;
+parameter ERR   = 2'b11;
 
-reg [3:0] state;
-reg [3:0] next;
-/*}}}*/
-
-// Combinational logic/*{{{*/
-//assign MData = write_data;
+reg [2:0] state;
+reg [2:0] next;
 /*}}}*/
 
 // State transition logic/*{{{*/
 always @(posedge Clk) begin
   if (EnableClk) begin
     if (reset) begin
-      state <= 4'b0;
+      state <= 3'b0;
       state[IDLE] <= 1'b1;
     end
 
@@ -98,9 +93,36 @@ end
 /*}}}*/
 
 // Next state logic/*{{{*/
-always @(state or read_request or write_request or SCmdAccept) begin
-  next <= 4'b0;
+always @(state or read_request or write_request or SCmdAccept or SResp or SData) begin
+  next <= 3'b0;
 
+  // Handle slave response/*{{{*/
+  case (SResp)
+    // No response
+    NULL: begin
+      read_data <= `SDATA_WIDTH'bx;
+    end
+
+    // Data valid / accept
+    DVA: begin
+      read_data <= SData;
+    end
+
+    // Request failed
+    FAIL: begin
+      read_data <= SData;
+    end
+
+    // Response error
+    ERR: begin
+      read_data <= `SDATA_WIDTH'bx;
+    end
+
+    default: begin
+      read_data <= `SDATA_WIDTH'bx;
+    end
+  endcase
+/*}}}*/
 
   // Handle read or write requests/*{{{*/
   case (1'b1)
@@ -151,7 +173,7 @@ always @(state or read_request or write_request or SCmdAccept) begin
       next[IDLE] <= 1'b1;
     end
   endcase
-/*}}}*/
+  /*}}}*/
 end
 /*}}}*/
 
@@ -159,75 +181,41 @@ end
 always @(posedge Clk) begin
   if (reset) begin
     MAddr <= `MADDR_WIDTH'bx;
-    MCmd <= 3'b0;
+    MCmd <= IDLE;
     MData <= `MDATA_WIDTH'bx;
-    MDataValid <= 1'b0;
-    read_data <= `MDATA_WIDTH'bx;
   end
 
   else begin
-    MCmd <= 3'b0;
-    
+    MCmd <= IDLE;
+
     // Handle Master outputs/*{{{*/
     case (1'b1)
       next[IDLE]: begin
         MAddr <= `MADDR_WIDTH'bx;
         MCmd <= IDLE;
         MData <= `MDATA_WIDTH'bx;
-        MDataValid <= 1'b0;
       end
 
       next[WR]: begin
         MAddr <= address;
         MCmd <= WR;
         MData <= write_data;
-        MDataValid <= 1'b1;
       end
 
       next[RD]: begin
         MAddr <= address;
         MCmd <= RD;
         MData <= `MDATA_WIDTH'bx;
-        MDataValid <= 1'b0;
-        end
-
-        default: begin
-          MAddr <= `MADDR_WIDTH'bx;
-          MCmd <= IDLE;
-          MData <= `MDATA_WIDTH'bx;
-          MDataValid <= 1'b0;
-        end
-      endcase
-/*}}}*/
-
-    // Handle slave response/*{{{*/
-    case (SResp)
-      // No response
-      NULL: begin
-        read_data <= `MDATA_WIDTH'bx;
-      end
-
-      // Data valid / accept
-      DVA: begin
-        read_data <= SData;
-      end
-
-      // Request failed
-      FAIL: begin
-        read_data <= SData;
-      end
-
-      // Response error
-      ERR: begin
-        read_data <= `MDATA_WIDTH'bx;
       end
 
       default: begin
-        read_data <= `MDATA_WIDTH'bx;
+        MAddr <= `MADDR_WIDTH'bx;
+        MCmd <= IDLE;
+        MData <= `MDATA_WIDTH'bx;
       end
     endcase
     /*}}}*/
-    end
   end
-  /*}}}*/
-  endmodule
+end
+/*}}}*/
+endmodule
