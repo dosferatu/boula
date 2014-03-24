7e130a89f953c413056464bc46d36d3d61676bed
Michael Walton <spesialstyrker@gmail.com>
2014-03-24 14:30:16 -0700

    Demonstrate simple read and write in master TB

--- src/ocp_master_fsm_tb.v
+++ src/ocp_master_fsm_tb.v
@@ -31,7 +31,7 @@ module ocp_master_fsm_tb();
   wire [`MADDR_WIDTH - 1:0] MAddr;
   wire [2:0]                MCmd;
   wire [`MDATA_WIDTH - 1:0] MData;
-  wire                      MDataValid;
+  //wire                      MDataValid;
   /*}}}*/
 
 // Declarations/*{{{*/
@@ -47,13 +47,13 @@ parameter BCST  = 3'b111;
 
 // SResp states
 parameter NULL  = 2'b00;
-parameter DVA   = 2'b00;
-parameter FAIL  = 2'b00;
-parameter ERR   = 2'b00;
+parameter DVA   = 2'b01;
+parameter FAIL  = 2'b10;
+parameter ERR   = 2'b11;
 
 ocp_master_fsm U0(
   .address(address),
-  .data_valid(data_valid),
+  //.data_valid(data_valid),
   .read_request(read_request),
   .reset(reset),
   .write_data(write_data),
@@ -63,34 +63,34 @@ ocp_master_fsm U0(
   .EnableClk(EnableClk),
   .SCmdAccept(SCmdAccept),
   .SData(SData),
-  .SDataAccept(SDataAccept),
+  //.SDataAccept(SDataAccept),
   .SResp(SResp),
   .MAddr(MAddr),
   .MCmd(MCmd),
-  .MData(MData),
-  .MDataValid(MDataValid)
+  .MData(MData)
+  //.MDataValid(MDataValid)
 );
 /*}}}*/
 
 // Master controller initialization/*{{{*/
 initial begin
   // Bridge simulation initialization
-  address 			<= `MADDR_WIDTH'b0;
-  data_valid 		<= 1'b0;
-  read_request 	<= 1'b0;
-  reset 				<= 1'b1;
-  write_data 		<= `MDATA_WIDTH'b0;
-  write_request	<= 1'b0;
+  address       <= `MADDR_WIDTH'bx;
+  data_valid    <= 1'b0;
+  read_request  <= 1'b0;
+  reset         <= 1'b1;
+  write_data    <= `MDATA_WIDTH'bx;
+  write_request <= 1'b0;
 
   // OCP interface initialization
-  Clk 			<= 1'b0;
-  EnableClk 	<= 1'b0;
-  SCmdAccept 	<= 1'b0;
-  SData 			<= `SDATA_WIDTH'b0;
-  SDataAccept 	<= 1'b0;
-  SResp 			<= NULL;
+  Clk           <= 1'b0;
+  EnableClk     <= 1'b0;
+  SCmdAccept    <= 1'b0;
+  SData         <= `SDATA_WIDTH'bx;
+  SDataAccept   <= 1'b0;
+  SResp         <= NULL;
   
-  // Start the clock
+  // Start the clock with a 10 unit period
   forever begin
     #10 Clk <= ~Clk;
   end
@@ -99,8 +99,67 @@ end
 
 // Test bench stimuli/*{{{*/
 initial begin
-  #20 EnableClk <= 1'b1;
-  #40 write_request <= 1'b1;
+  #20 reset <= 1'b0;
+  EnableClk <= 1'b1;
+
+  // Perform a simple write to the slave/*{{{*/
+  
+  // WR state
+  // Request phase
+  #50 address <= 64'hFFFFFFFFFFFFFFFF;
+  read_request <= 1'b0;
+  write_data <= `MDATA_WIDTH'hFF;
+  write_request <= 1'b1;
+  #20 SCmdAccept <= 1'b1;
+
+  // Response phase
+  SResp <= NULL;
+  SData <= `SDATA_WIDTH'bx;
+
+
+  // IDLE state
+  // Request phase
+  #20 address <= 64'bx;
+  read_request <= 1'b0;
+  write_data <= `MDATA_WIDTH'bx;
+  write_request <= 1'b0;
+  SCmdAccept <= 1'b0;
+
+  // Response phase
+  SResp <= NULL;
+  SData <= `SDATA_WIDTH'bx;
+/*}}}*/
+
+
+  // Perform a simple read request to the slave/*{{{*/
+  
+  // RD state
+  // Request phase
+  #60 address <= 64'hFFFFFFFFFFFFFFFF;
+  read_request <= 1'b1;
+  write_data <= `MDATA_WIDTH'bx;
+  write_request <= 1'b0;
+  #20 SCmdAccept <= 1'b1;
+
+  // Response phase
+  SResp <= NULL;
+  SData <= `SDATA_WIDTH'bx;
+
+
+  // IDLE state
+  // Request phase
+  #20 address <= 64'bx;
+  read_request <= 1'b0;
+  write_data <= `MDATA_WIDTH'bx;
+  write_request <= 1'b0;
+  SCmdAccept <= 1'b0;
+
+  // Response phase
+  SResp <= DVA;
+  SData <= `SDATA_WIDTH'hFF;
+  #20 SResp <= NULL;
+  SData <= `SDATA_WIDTH'bx;
+/*}}}*/
 end
 /*}}}*/
 endmodule
