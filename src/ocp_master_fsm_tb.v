976620d09ec52e792754a7314a40f68c97f36069
Michael Walton <spesialstyrker@gmail.com>
2014-03-26 17:25:16 -0700

    Added tests for r/w sinqle request bursts

--- src/ocp_master_fsm_tb.v
+++ src/ocp_master_fsm_tb.v
@@ -13,6 +13,7 @@ module ocp_master_fsm_tb();
   reg [`MADDR_WIDTH - 1:0]  address;
   reg [9:0]                 burst_length;
   reg [2:0]                 burst_seq;
+  reg                       burst_single_req;
   reg                       data_valid;
   reg                       read_request;
   reg                       reset;
@@ -66,6 +67,7 @@ ocp_master_fsm U0(
   .address(address),
   .burst_length(burst_length),
   .burst_seq(burst_seq),
+  .burst_single_req(burst_single_req),
   //.data_valid(data_valid),
   .read_request(read_request),
   .reset(reset),
@@ -91,23 +93,24 @@ ocp_master_fsm U0(
 // Master controller initialization/*{{{*/
 initial begin
   // Bridge simulation initialization
-  address       <= `MADDR_WIDTH'bx;
-  burst_length  <= 1'b1;
-  burst_seq     <= 2'b00;
-  data_valid    <= 1'b0;
-  read_request  <= 1'b0;
-  reset         <= 1'b1;
-  write_data    <= `MDATA_WIDTH'bx;
-  write_request <= 1'b0;
+  address           <= `MADDR_WIDTH'bx;
+  burst_length      <= 1'b1;
+  burst_seq         <= 2'b00;
+  burst_single_req  <= 1'b0;
+  data_valid        <= 1'b0;
+  read_request      <= 1'b0;
+  reset             <= 1'b1;
+  write_data        <= `MDATA_WIDTH'bx;
+  write_request     <= 1'b0;
 
   // OCP interface initialization
-  clk           <= 1'b0;
-  EnableClk     <= 1'b0;
-  SCmdAccept    <= 1'b0;
-  SData         <= `SDATA_WIDTH'bx;
-  SDataAccept   <= 1'b0;
-  SResp         <= NULL;
-  SResp         <= 1'b0;
+  clk               <= 1'b0;
+  EnableClk         <= 1'b0;
+  SCmdAccept        <= 1'b0;
+  SData             <= `SDATA_WIDTH'bx;
+  SDataAccept       <= 1'b0;
+  SResp             <= NULL;
+  SResp             <= 1'b0;
   
   // Start the clock with a 10 unit period
   forever begin
@@ -200,6 +203,78 @@ initial begin
 /*}}}*/
 
 
+// Perform a single request burst write to the slave/*{{{*/
+
+  // Request phase (IMPLEMENT DATA HANDSHAKE IN CONTROLLER)
+  #60 write_request     <= 1'b0;  // SET WHEN D.H. PHASE CONTROL IMPLEMENTED
+  read_request          <= 1'b0;
+  #20 SCmdAccept        <= 1'b1;
+  address               <= 64'hFF;
+  burst_length          <= 10'h5;
+  burst_single_req      <= 1'b1;
+
+  // Data handshake phase
+  write_data            <= `MDATA_WIDTH'h4;
+  SDataAccept           <= 1'b1;
+  #20 SCmdAccept        <= 1'b0;
+  address               <= 64'b0;
+  burst_single_req      <= 1'b0;
+  write_data            <= `MDATA_WIDTH'h8;
+  #20 write_data        <= `MDATA_WIDTH'hC;
+  #20 write_data        <= `MDATA_WIDTH'h20;
+  #20 write_data        <= `MDATA_WIDTH'h24;
+  
+  // Finish stimuli
+  #20 address <= 64'b0;
+  burst_length <= 10'b0;
+  read_request <= 1'b0;
+  write_data <= `MDATA_WIDTH'bx;
+  write_request <= 1'b0;
+  SCmdAccept <= 1'b0;
+  SResp <= NULL;
+  SRespLast <= 1'bx;
+  SData <= `SDATA_WIDTH'bx;
+/*}}}*/
+
+
+// Perform a single request burst read to the slave/*{{{*/
+
+  // Request phase
+  #60 read_request      <= 1'b1;
+  write_request         <= 1'b0;
+  #20 SCmdAccept        <= 1'b1;
+  address               <= 64'hFF;
+  burst_length          <= 10'h7;
+  burst_single_req      <= 1'b1;
+  read_request          <= 1'b0;
+  #20 SCmdAccept        <= 1'b0;
+  burst_single_req      <= 1'b0;
+  
+  // Response phase
+  #20 SResp             <= DVA;
+  SRespLast             <= 1'b0;
+  SData                 <= `MDATA_WIDTH'h1C;
+  #20 SData             <= `MDATA_WIDTH'h18;
+  #20 SData             <= `MDATA_WIDTH'h14;
+  #20 SData             <= `MDATA_WIDTH'h10;
+  #20 SData             <= `MDATA_WIDTH'h0C;
+  #20 SData             <= `MDATA_WIDTH'h08;
+  #20 SData             <= `MDATA_WIDTH'h04;
+  SRespLast             <= 1'b1;
+
+  // Finish stimuli
+  #20 address <= 64'b0;
+  burst_length <= 10'b0;
+  read_request <= 1'b0;
+  write_data <= `MDATA_WIDTH'bx;
+  write_request <= 1'b0;
+  SCmdAccept <= 1'b0;
+  SResp <= NULL;
+  SRespLast <= 1'bx;
+  SData <= `SDATA_WIDTH'bx;
+/*}}}*/
+
+
   // Perform an incrementing precise burst write to the slave/*{{{*/
 
   // WR state
@@ -240,7 +315,7 @@ initial begin
   /*}}}*/
 
 
-  // Perform an incrementing precise burst read request to the slave/*{{{*/
+  // Perform an incrementing precise burst read from the slave/*{{{*/
 
   // RD state
   // Request phase (Data 1)
