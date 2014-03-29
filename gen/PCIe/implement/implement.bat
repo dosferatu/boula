dda24b87d408bda16aca3dcb55c6a1a6b3956209
Michael Walton <spesialstyrker@gmail.com>
2014-03-29 12:36:21 -0700

    Initial PCIe coregen module

---
+++ gen/PCIe/implement/implement.bat
@@ -0,0 +1,32 @@
+rem Clean up the results directory
+rmdir /S /Q results
+mkdir results
+
+echo 'Synthesizing HDL example design with XST';
+xst -ifn xilinx_pcie_2_0_ep_v6.xst -ofn xilinx_pcie_2_0_ep_v6.log
+rem xst -ifn xst.scr
+
+copy xilinx_pcie_2_0_ep_v6.ngc .\results\
+
+copy xilinx_pcie_2_0_ep_v6.log xst.srp
+
+cd results
+
+echo 'Running ngdbuild'
+ngdbuild -verbose -uc ../../example_design/xilinx_pcie_2_0_ep_v6_01_lane_gen1_xc6vlx75t-ff484-3-PCIE_X0Y0.ucf xilinx_pcie_2_0_ep_v6.ngc -sd .
+
+echo 'Running map'
+map -w -o mapped.ncd xilinx_pcie_2_0_ep_v6.ngd mapped.pcf
+
+echo 'Running par'
+par -w mapped.ncd routed.ncd mapped.pcf
+
+echo 'Running trce'
+trce -u -e 100 routed.ncd mapped.pcf
+
+echo 'Running design through netgen'
+netgen -sim -ofmt verilog -ne -w -tm xilinx_pcie_2_0_ep_v6 -sdf_path . routed.ncd
+
+echo 'Running design through bitgen'
+bitgen -w routed.ncd
+
---
+++ gen/PCIe_xmdf.tcl
@@ -0,0 +1,152 @@
+# The package naming convention is <core_name>_xmdf
+package provide PCIe_xmdf 1.0
+
+# This includes some utilities that support common XMDF operations
+package require utilities_xmdf
+
+# Define a namespace for this package. The name of the name space
+# is <core_name>_xmdf
+namespace eval ::PCIe_xmdf {
+# Use this to define any statics
+}
+
+# Function called by client to rebuild the params and port arrays
+# Optional when the use context does not require the param or ports
+# arrays to be available.
+proc ::PCIe_xmdf::xmdfInit { instance } {
+# Variable containg name of library into which module is compiled
+# Recommendation: <module_name>
+# Required
+utilities_xmdf::xmdfSetData $instance Module Attributes Name PCIe}
+# ::PCIe_xmdf::xmdfInit
+
+# Function called by client to fill in all the xmdf* data variables
+# based on the current settings of the parameters
+proc ::PCIe_xmdf::xmdfApplyParams { instance } {
+
+set fcount 0
+# Array containing libraries that are assumed to exist
+# Examples include unisim and xilinxcorelib
+# Optional
+# In this example, we assume that the unisim library will
+# be magically
+# available to the simulation and synthesis tool
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type logical_library
+utilities_xmdf::xmdfSetData $instance FileSet $fcount logical_library unisim
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path PCIe/source/gtx_tx_sync_rate_v6.v
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path PCIe/source/gtx_wrapper_v6.v
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path PCIe/source/gtx_rx_valid_filter_v6.v
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path PCIe/source/pcie_upconfig_fix_3451_v6.v
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path PCIe/source/pcie_2_0_v6.v
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path PCIe/source/pcie_bram_top_v6.v
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path PCIe/source/pcie_bram_v6.v
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path PCIe/source/pcie_brams_v6.v
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path PCIe/source/pcie_clocking_v6.v
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path PCIe/source/pcie_gtx_v6.v
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path PCIe/source/pcie_pipe_lane_v6.v
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path PCIe/source/pcie_pipe_misc_v6.v
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path PCIe/source/pcie_pipe_v6.v
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path PCIe/source/pcie_reset_delay_v6.v
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path PCIe/source/axi_basic_tx_thrtl_ctl.v
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path PCIe/source/axi_basic_rx.v
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path PCIe/source/axi_basic_rx_null_gen.v
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path PCIe/source/axi_basic_rx_pipeline.v
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path PCIe/source/axi_basic_tx_pipeline.v
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path PCIe/source/axi_basic_tx.v
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path PCIe/source/axi_basic_top.v
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path PCIe/source/PCIe.v
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog
+utilities_xmdf::xmdfSetData $instance FileSet $fcount toplevel true
+incr fcount
+
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path PCIe/example_design/xilinx_pcie_2_0_ep_v6_01_lane_gen1_xc6vlx75t-ff484-3-PCIE_X0Y0.ucf
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type Ucf
+utilities_xmdf::xmdfSetData $instance FileSet $fcount associated_module xilinx_pcie_2_0_ep_v6.v
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path PCIe.veo
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog_template
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path PCIe.xco
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type coregen_ip
+incr fcount
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path PCIe_xmdf.tcl
+utilities_xmdf::xmdfSetData $instance FileSet $fcount type AnyView
+incr fcount
+
+
+utilities_xmdf::xmdfSetData $instance FileSet $fcount associated_module PCIe
+incr fcount
+
+}
+
+# ::PCIe_xmdf::xmdfApplyParams
+
