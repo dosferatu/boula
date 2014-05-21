7a6b57ef661822fa39a6d35f9daa169103ab6ab6
bhunt2 <bhunt2@pdx.edu>
2014-05-21 08:49:32 -0700

    updated control again by removing ready signals

--- src/control.v
+++ src/control.v
@@ -16,7 +16,6 @@
 
         // Outputs of module
         output reg [2:0] Ctrl_fc_sel,    // Selection of desired flow control type
-        output           Ctrl_Bridge_Ready, // Signals to the rest of the bridge that operation is ok
         output           Ctrl_Bridge_RST,// Signals to Bridge that a reset is needed
         output [5:0]     Ctrl_Tx_FC      // Signals for Flow Credits to transmit bridge module
         );
@@ -34,15 +33,15 @@
         //  Control should signal that it is not ok to transmit a given packet
         //  type once less than one maximum size packet can fit.
         always begin
-                if (Ctrl_fc_ph [7:5] == 0)        Ctrl_Tx_FC[0] <= 0;
+                if (Ctrl_fc_ph [7:5] == 0)      Ctrl_Tx_FC[0] <= 0;
                         else Ctrl_Tx_FC[0] <= 1;
                 if (Ctrl_fc_ph[11:10] == 0)     Ctrl_Tx_FC[1] <= 0;
                         else Ctrl_Tx_FC[1] <= 1;
-                if (Ctrl_fc_nph[7:5] == 0)        Ctrl_Tx_FC[2] <= 0;
+                if (Ctrl_fc_nph[7:5] == 0)      Ctrl_Tx_FC[2] <= 0;
                         else Ctrl_Tx_FC[2] <= 1;
                 if (Ctrl_fc_npd[11:10] == 0)    Ctrl_Tx_FC[3] <= 0;
                         else Ctrl_Tx_FC[3] <= 1;
-                if (Ctrl_fc_cplh[7:5] == 0)       Ctrl_Tx_FC[4] <= 0;
+                if (Ctrl_fc_cplh[7:5] == 0)     Ctrl_Tx_FC[4] <= 0;
                         else Ctrl_Tx_FC[4] <= 1;
                 if (Ctrl_fc_cpld[11:10] == 0)   Ctrl_Tx_FC[5] <= 0;
                         else Ctrl_Tx_FC[5] <= 1;
