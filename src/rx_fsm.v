82ffb18dacec7ca66de83ada65605245b5657777
bhunt2 <bhunt2@pdx.edu>
2014-05-26 11:25:42 -0700

    made some more updates to rx module

--- src/rx_fsm.v
+++ src/rx_fsm.v
@@ -18,13 +18,16 @@ module rx_header_fsm(
     input wire rx_header_valid,
     input wire [keep_width - 1:0] rx_header_keep,
     input wire rx_header_last,
-    output reg rx_heaer_ready,
+    output reg rx_header_ready,
     /*}}}*/
 
     // Tx AXI FIFO/*{{{*/
     output wire tx_header_fifo_valid,
     input  wire tx_header_fifo_ready
     /*}}}*/
+
+    // OCP Registers/*{{{*/
+    output reg [2:0] ocp_reg_ctl;
     );
 
     // Declarations/*{{{*/
@@ -37,10 +40,11 @@ module rx_header_fsm(
     reg is4;    // Indicates that 64 bit addressing is used so the header is 4DW, not 3DW
 
     // State encodings
-    localparam IDLE = 2'b00;
-    localparam H1   = 2'b01;
-    localparam H2   = 2'b10;
-    localparam DATA = 2'b11;
+    localparam IDLE     = 3'b000;
+    localparam H1       = 3'b001;
+    localparam H2       = 3'b010;
+    localparam DATA3    = 3'b011;
+    localparam DATA4    = 3'b100;
     
     // Format encoding for PCI Express 2.0
     localparam MRD3 = 3'b000; // 3DW header, no data
@@ -58,10 +62,9 @@ module rx_header_fsm(
     // State transition logic/*{{{*/
     always @(posedge clk) begin
         if (reset) begin
-            state <= 4'b0;              // Reset state value to zero
-            state[IDLE] <= 1'b1;        // Set to IDLE state
+            state <= 4'b0;          // Reset state value to zero
+            state[RESET] <= 1'b1;   // Set to RESET state
         end
-
         else begin
             state <= next;              // Transition to next state
         end
@@ -73,344 +76,126 @@ module rx_header_fsm(
         next <= 4'b0;
 
         case (1'b1)
+            // IDLE/*{{{*/
+            //  System Idles while waiting for valid TLP to be presented
+            //  Stays until rx_header_valid and tx_header_fifo_ready asserted
             state[IDLE]: begin
                 if (rx_header_valid && tx_header_fifo_ready) begin
-                    next[H1]    = 1'b1; end   // TLP ready to be received from AXI-4 Stream bus
+                    next[H1]    = 1'b1; end     // TLP ready to receive
                 else begin
-                    next[IDLE]  = 1'b1; end   // Not valid so stay
+                    next[IDLE]  = 1'b1; end     // Not valid so stay
             end
+            /*}}}*/
 
+            // H1/*{{{*/
+            //  First header slice transmitted
+            //  Stays unless PCIe Core holds valid high and tx header fifo is
+            //  ready
             state[H1]: begin
-                if (rx_header_valid && tx_head_fifo_ready) begin
-                    next[H2]    = 1'b1; end   // Calculate the request type
+                if (rx_header_valid && tx_header_fifo_ready) begin
+                    next[H2]    = 1'b1; end     // Received first slice, move to second
                 else begin 
-                    next[H1]    = 1'b1; end   // Not valid so stay
+                    next[H1]    = 1'b1; end     // Not valid so stay
             end
+            /*}}}*/
 
+            // H2/*{{{*/
+            // Second header slice transmitted
             state[H2]: begin
-                if (rx_header_valid && isdata)
-                                        next[DATA]  = 1'b1;    // Header complete and data present (write op)
-                else if(rx_header_valid && ~isdata)
-                                        next[IDLE]  = 1'b1;    // Header complete and no data (read op)
-                else                    next[H2]    = 1'b1;    // Not valid so stay
+                if (rx_header_valid && tx_header_fifo_ready && isdata) begin
+                    next[DATA]  = 1'b1; end     // Header complete and data present (write op)
+                else if(rx_header_valid && tx_header_fifo_ready && ~isdata) begin
+                    next[IDLE]  = 1'b1; end     // Header complete and no data (read op)
+                else begin 
+                    next[H2]    = 1'b1; end     // Not valid so stay
             end
+            /*}}}*/
 
-            state[DATA]: begin
-                if (rx_header_valid && rx_header_last)
-                                        next[IDLE]  <= 1'b1;    // Written last data, finish operation
-                else                    next[DATA]  <= 1'b1;    // Still data left to transmit
+            // Data transmission/*{{{*/
+            // Controls transmission of data on a 96 bit shift register
+            state[DATA3]: begin
+                if (rx_header_valid && tx_header_fifo_ready && rx_header_last) begin
+                    next[IDLE]  = 1'b1; end     // Written last data, finish operation
+                else if (rx_header_valid && tx_header_fifo_ready && isdata) begin
+                    next[DATA3]  = 1'b1; end     // Still data left to transmit
             end
+            state[DATA4]: begin
+                if (rx_header_valid && rx_header_last) begin
+                    next[IDLE]  = 1'b1; end     // Written last data, finish operation
+                else begin
+                    next[DATA]  = 1'b1; end     // Still data left to transmit
+            end
+            //*}}}*/
 
-            default:                    next[IDLE] <= 1'b1;     // If nothing matches return to default
+            default: begin 
+                    next[IDLE]  = 1'b1; end     // If nothing matches return to default
         endcase
     end
     /*}}}*/
 
     // Output logic/*{{{*/
-    always @(posedge clk) begin
-        // RESET/*{{{*/
-        if (reset) begin
-            //  input
-            m_aclk              <= 1'b0;
-            m_axis_tready       <= 1'b0;
-
-            // OCP 2.2 Interface/*{{{*/
-
-            address             <= {`addr_wdth{1'b0}};
-            enable              <= 1'b0;
-            burst_seq           <= INCR;
-            burst_single_req    <= 1'b0;
-            burst_length        <= 1'b1;
-            data_valid          <= 1'b0;
-            read_request        <= 1'b0;
-            ocp_reset           <= 1'b0;
-            sys_clk             <= 1'b0;
-            write_data          <= {`data_wdth{1'b0}};
-            write_request       <= 1'b0;
-            writeresp_enable    <= 1'b0;
+    always @(posedge rx_header_clk) begin
+        case (1'b1)
+            // IDLE/*{{{*/
+            //  Then it asserts rx_header_ready for transmission to begin
+            next[IDLE]: begin
+                rx_header_ready         = 1'b0; // Core AXI Interface - Bridge not ready
+                tx_header_fifo_valid    = 1'b0; // Tx header FIFO - No data 
+                ocp_reg_ctl             = 3'b0; // OCP Register Control
+            end
             /*}}}*/
 
-            // Bridge internals/*{{{*/
-
-            header_counter <= 2'b0;
-            data_counter <= 2'b0;
-            tlp_data_length <= 10'b0;
-            tlp_format <= 3'b0;
-            tlp_header_length <= 2'b0;
-            tlp_type <= 5'b0;
-            tlp_header_0 <= 64'b0;
-            tlp_header_1 <= 64'b0;
-            tlp_header_2 <= 64'b0;
-            tlp_header_3 <= 64'b0;
-        end
-        /*}}}*/
-        /*}}}*/
-
-        else begin
-            case (1'b1)
-                // IDLE/*{{{*/
-                next[IDLE]: begin
-                    // AXI FIFO input
-                    m_aclk          <= 1'b0;
-                    m_axis_tready   <= 1'b1;  // Ready to receive on the AXI-4 Stream Bus
-
-                    // OCP 2.2 Interface/*{{{*/
-
-                    address             <= {`addr_wdth{1'b0}};
-                    enable              <= 1'b1;
-                    burst_seq           <= INCR;
-                    burst_single_req    <= 1'b0;
-                    burst_length        <= 1'b1;
-                    data_valid          <= 1'b0;
-                    read_request        <= 1'b0;
-                    ocp_reset           <= 1'b0;
-                    sys_clk             <= 1'b0;
-                    write_data          <= {`data_wdth{1'b0}};
-                    write_request       <= 1'b0;
-                    writeresp_enable    <= 1'b0;
+            // First TLP Header Slice/*{{{*/
+            //  
+            next[H1]: begin
+                if (rx_header_valid && tx_header_fifo_ready) begin
+                    // AXI Inteface/*{{{*/
+                    rx_header_ready         = 1'b0;
                     /*}}}*/
 
-                    // Bridge internals/*{{{*/
-
-                    header_counter <= 2'b0;
-                    data_counter <= 2'b0;
-                    tlp_data_length <= 10'b0;
-                    tlp_format <= 3'b0;
-                    tlp_header_length <= 2'b0;
-                    tlp_type <= 5'b0;
-                    tlp_header_0 <= 64'b0;
-                    tlp_header_1 <= 64'b0;
-                    tlp_header_2 <= 64'b0;
-                    tlp_header_3 <= 64'b0;
-                end
-                /*}}}*/
-                /*}}}*/
-
-                // REQUEST/*{{{*/
-                next[REQUEST]: begin
-                    // AXI FIFO input
-                    m_aclk              <= 1'b0;
-                    m_axis_tready <= 1'b0;  // Notify the AXI FIFO we are ready for more data
-
-                    // OCP 2.2 Interface/*{{{*/
-
-                    address             <= {`addr_wdth{1'b0}};
-                    enable              <= 1'b1;
-                    burst_seq           <= INCR;
-                    burst_single_req    <= 1'b0;
-                    burst_length        <= 1'b1;
-                    data_valid          <= 1'b0;
-                    read_request        <= 1'b0;
-                    ocp_reset           <= 1'b0;
-                    sys_clk             <= 1'b0;
-                    write_data          <= {`data_wdth{1'b0}};
-                    write_request       <= 1'b0;
-                    writeresp_enable    <= 1'b0;
+                    // Tx Header FIFO/*{{{*/
+                    tx_header_fifo_valid    = 1'b0;
                     /*}}}*/
 
-                    // Bridge internals/*{{{*/
-
-                    case(m_axis_tdata[7:5])
-                        MRD: begin
-                            tlp_header_length <= 2'b10;
-                        end
-
-                        MRDLK: begin
-                            tlp_header_length <= 2'b11;
-                        end
-
-                        MWR: begin
-                            tlp_header_length <= 2'b10;
-                        end
-
-                        MWR2: begin
-                            tlp_header_length <= 2'b11;
-                        end
-
-                        PRFX: begin
-                            tlp_header_length <= 2'b10;
-                        end
-
-                        default: begin
-                            tlp_header_length <= 2'b0;
-                        end
-                    endcase
-
-                    data_counter <= 10'b0;
-                    header_counter <= 2'b0;
-                    tlp_data_length <= m_axis_tdata[31:24];
-                    tlp_format <= m_axis_tdata[7:5];
-                    tlp_type <= m_axis_tdata[4:0];
-                    tlp_header_0 <= m_axis_tdata;
-                    tlp_header_1 <= 64'b0;
-                    tlp_header_2 <= 64'b0;
-                    tlp_header_3 <= 64'b0;
-                end
+                    // OCP Register controls/*{{{*/
+                ocp_reg_ctl             = 3'b0;
                 /*}}}*/
+            end
+            /*}}}*/
+
+            // Second TLP Header Slice/*{{{*/
+            next[H2]: begin
+                // AXI Inteface/*{{{*/
+                rx_header_ready         = 1'b0;
                 /*}}}*/
 
-                // PROC/*{{{*/
-                next[PROC]: begin
-                    // AXI FIFO input
-                    //m_aclk              <= 1'b0;
-                    m_axis_tready       <= 1'b1;
+                // Tx Header FIFO/*{{{*/
+                tx_header_fifo_valid    = 1'b0;
+                /*}}}*/
 
-                    // OCP 2.2 Interface/*{{{*/
+                // OCP Register controls/*{{{*/
+                ocp_reg_ctl             = 3'b0;
+                /*}}}*/
+            end
+            /*}}}*/
 
-                    address             <= {`addr_wdth{1'b0}};
-                    enable              <= 1'b1;
-                    burst_seq           <= INCR;
-                    burst_single_req    <= 1'b0;
-                    burst_length        <= 1'b1;
-                    data_valid          <= 1'b0;
-                    read_request        <= 1'b0;
-                    ocp_reset           <= 1'b0;
-                    sys_clk             <= 1'b0;
-                    write_data          <= {`data_wdth{1'b0}};
-                    write_request       <= 1'b0;
-                    writeresp_enable    <= 1'b0;
-                    /*}}}*/
+            // Transmit data/*{{{*/
+            next[DATA]: begin
+                // AXI Inteface/*{{{*/
+                rx_header_ready         = 1'b0;
+                /*}}}*/
 
-                    // Bridge internals/*{{{*/
+                // Tx Header FIFO/*{{{*/
+                tx_header_fifo_valid    = 1'b0;
+                /*}}}*/
 
-                    data_counter <= 10'b0;
-                    header_counter <= m_axis_tvalid ? header_counter + 1'b1 : data_counter;
-                    tlp_data_length <= tlp_data_length;
-                    tlp_format <= m_axis_tdata[7:5];
-                    tlp_header_length <= tlp_header_length;
-                    tlp_type <= m_axis_tdata[4:0];
-                    tlp_header_0 <= m_axis_tdata;
-
-                    /*
-                    * Assign the next header packet if FIFO has valid data.
-                        * If there's no valid data ready then do nothing.
-                        */
-                       if (m_axis_tvalid) begin
-                           case (header_counter)
-                               0: begin
-                                   tlp_header_0 <= tlp_header_0;
-                                   tlp_header_1 <= 64'b0;
-                                   tlp_header_2 <= 64'b0;
-                                   tlp_header_3 <= 64'b0;
-                               end
-
-                               1: begin
-                                   tlp_header_0 <= tlp_header_0;
-                                   tlp_header_1 <= m_axis_tdata;
-                                   tlp_header_2 <= 64'b0;
-                                   tlp_header_3 <= 64'b0;
-                               end
-
-                               2: begin
-                                   tlp_header_0 <= tlp_header_0;
-                                   tlp_header_1 <= tlp_header_1;
-                                   tlp_header_2 <= m_axis_tdata;
-                                   tlp_header_3 <= 64'b0;
-                               end
-
-                               3: begin
-                                   tlp_header_0 <= tlp_header_0;
-                                   tlp_header_1 <= tlp_header_1;
-                                   tlp_header_2 <= tlp_header_2;
-                                   tlp_header_3 <= m_axis_tdata;
-                               end
-
-                               default: begin
-                                   tlp_header_0 <= tlp_header_0;
-                                   tlp_header_1 <= 64'b0;
-                                   tlp_header_2 <= 64'b0;
-                                   tlp_header_3 <= 64'b0;
-                               end
-                           endcase
-                       end
-
-                       else begin
-                           tlp_header_0 <= tlp_header_0;
-                           tlp_header_1 <= tlp_header_1;
-                           tlp_header_2 <= tlp_header_2;
-                           tlp_header_3 <= tlp_header_3;
-                       end
-                   end
-                   /*}}}*/
-                   /*}}}*/
-
-                   // EXEC/*{{{*/
-                   next[EXEC]: begin
-                       // AXI FIFO input
-                       m_aclk              <= 1'b0;
-                       m_axis_tready       <= 1'b0;
-
-                       // OCP 2.2 Interface/*{{{*/
-
-                       address             <= {`addr_wdth{1'b0}};  // Set this using the base address register + counter offset
-                       enable              <= 1'b1;
-                       burst_seq           <= INCR;
-                       burst_single_req    <= 1'b0;
-                       burst_length        <= 1'b1;
-                       data_valid          <= 1'b0;
-                       read_request        <= ~(tlp_format & 3'b010);
-                       ocp_reset           <= 1'b0;
-                       sys_clk             <= 1'b0;
-                       write_data          <= (write_request & m_axis_tvalid) ? m_axis_tdata : {`data_wdth{1'b0}};
-                       write_request       <= tlp_format & 3'b010; // Check if bit 2 is clear in the format register
-                       writeresp_enable    <= 1'b0;
-                       /*}}}*/
-
-                       // Bridge internals/*{{{*/
-
-                       data_counter <= m_axis_tvalid ? data_counter + 1'b1 : data_counter;
-                       header_counter <= 2'b0;
-                       tlp_data_length <= tlp_data_length;
-                       tlp_format <= tlp_format;
-                       tlp_header_length <= tlp_header_length;
-                       tlp_type <= tlp_type;
-                       tlp_header_0 <= tlp_header_0;
-                       tlp_header_1 <= tlp_header_1;
-                       tlp_header_2 <= tlp_header_2;
-                       tlp_header_3 <= tlp_header_3;
-                   end
-                   /*}}}*/
-                   /*}}}*/
-
-                   // DEFAULT/*{{{*/
-                   default: begin
-                       // AXI FIFO input
-                       m_aclk              <= 1'b0;
-                       m_axis_tready       <= 1'b0;
-
-                       // OCP 2.2 Interface/*{{{*/
-
-                       address             <= {`addr_wdth{1'b0}};
-                       enable              <= 1'b0;
-                       burst_seq           <= INCR;
-                       burst_single_req    <= 1'b0;
-                       burst_length        <= 1'b1;
-                       data_valid          <= 1'b0;
-                       read_request        <= 1'b0;
-                       ocp_reset           <= 1'b0;
-                       sys_clk             <= 1'b0;
-                       write_data          <= {`data_wdth{1'b0}};
-                       write_request       <= 1'b0;
-                       writeresp_enable    <= 1'b0;
-                       /*}}}*/
-
-                       // Bridge internals/*{{{*/
-
-                       header_counter <= 2'b0;
-                       data_counter <= 10'b0;
-                       tlp_data_length <= 10'b0;
-                       tlp_format <= 3'b0;
-                       tlp_header_length <= 2'b0;
-                       tlp_type <= 5'b0;
-                       tlp_header_0 <= m_axis_tdata;
-                       tlp_header_1 <= 64'b0;
-                       tlp_header_2 <= 64'b0;
-                       tlp_header_3 <= 64'b0;
-                   end
-                   /*}}}*/
-                   /*}}}*/
-               endcase
-           end
+                // OCP Register controls/*{{{*/
+                ocp_reg_ctl             = 3'b0;
+                /*}}}*/
+            end
+            /*}}}*/
+           endcase
        end
-       /*}}}*/
-       endmodule
+   end
+   /*}}}*/
+   endmodule
